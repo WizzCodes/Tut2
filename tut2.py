e0747e8e396b20af6403ec1d89713b6158c64cd3
Saurav Yadav <71329650+WizzCodes@users.noreply.github.com>
2021-05-11 22:15:00 +0530

    Add files via upload

---
+++ tut2.py
@@ -0,0 +1,17 @@
+var1 = ("36")
+var4 = ("24")
+var2 = 5
+var3 = 32.5
+#print(type(var1),type(var2),type(var3))
+#print(var2 + var3)
+#print(var1 + var4)
+#print(int(var1) + int(var4))
+#print(10 * 'hello world\n')
+print("Enter Your number")
+inpnum = input()
+print("Your entered number", int(inpnum)+2210)
+"""
+"str"
+"float"
+"int"
+"""
